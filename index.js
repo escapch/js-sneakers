b55a978142f8f5231432349227515ccb00f1ae68
escapch <bkbekmurzaev@gmail.com>
2023-11-29 14:37:57 +0600

    new search function (

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ let newSneakers = {
   imageUrl: '',
 };
 
+let inputTherm = '';
 const renderCartSneakers = async (cartArr) => {
   cartSneakers.innerHTML += `
             <div class="cart__item">
@@ -31,12 +32,27 @@ const renderCartSneakers = async (cartArr) => {
     
         `;
 };
-
-const render = async () => {
+searchInput.addEventListener('input', () => {
+  // Обновляем текст в элементе для отображения текущего значения
+  inputTherm = searchInput.value.toLowerCase();
+});
+const render = async (inputTherm) => {
   const response = await fetch('https://62fa813affd7197707ee0944.mockapi.io/items');
   const sneakers = await response.json();
-  sneakers.forEach((element) => {
-    sneakersContent.innerHTML += `
+
+  sneakersContent.innerHTML = '';
+
+  if (inputTherm) {
+    const items = sneakers
+      .filter((obj) => {
+        if (obj.title.toLowerCase().includes(inputTherm)) {
+          return true;
+        }
+
+        return false;
+      })
+      .map((element) => {
+        sneakersContent.innerHTML += `
             <div class="sneakers__card">
                 <button class="sneakers__favorit">
                     <img src="./img/favorit.svg" alt="no liked">
@@ -57,57 +73,82 @@ const render = async () => {
                 </div>
             </div>
         `;
-  });
-
-  searchInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      performSearch();
-    }
-  });
-  searchBtn.addEventListener('click', () => {
-    performSearch();
-  });
-
-  function performSearch() {
-    const searchTerm = searchInput.value;
-
-    const getSneakers = (word, sneakers) => {
-      return sneakers.filter((s) => {
-        const regex = new RegExp(word, 'gi');
-        return s.title.match(regex);
       });
-    };
-
-    const options = getSneakers(searchTerm, sneakers);
-    const html = options
-      .map((sneaker) => {
-        return `
-                <div class="sneakers__card">
-                    <button class="sneakers__favorit">
-                        <img src="./img/favorit.svg" alt="">
-                    </button>
-                    <div class="sneakers__img">
-                        <img src="${sneaker.imageUrl}" alt="sneakersIMG"/>
+  } else {
+    sneakers.forEach((element) => {
+      sneakersContent.innerHTML += `
+            <div class="sneakers__card">
+                <button class="sneakers__favorit">
+                    <img src="./img/favorit.svg" alt="no liked">
+                </button>
+                <div class="sneakers__img">
+                    <img src="${element.imageUrl}" alt="sneakersIMG"/>
+                </div>
+                <div class="sneakers-title">${element.title}</div>
+                <div class="sneakers__lower">
+                    <div class="sneakers__price">
+                        <span class="sneakers-price-title">Цена:</span>
+                        <p class="sneakers-price">${element.price} руб.</p>
                     </div>
-                    <div class="sneakers-title">${sneaker.title}</div>
-                    <div class="sneakers__lower">
-                        <div class="sneakers__price">
-                            <span class="sneakers-price-title">Цена:</span>
-                            <p class="sneakers-price">${sneaker.price} руб.</p>
-                        </div>
-                        <div class="sneacers__add">
-                            <button><img src="./img/addSneakers.svg" alt="">
-                            </button>
-                        </div>
+                    <div class="sneacers__add">
+                        <button class="addSneakers"><img src="./img/addSneakers.svg" alt="">
+                        </button>
                     </div>
                 </div>
-            `;
-      })
-      .join('');
-    sneakersContent.innerHTML = html;
-    searchInput.value = ''; // Очистите поле ввода
+            </div>
+        `;
+    });
   }
+
+  // searchInput.addEventListener('keydown', (e) => {
+  //   if (e.key === 'Enter') {
+  //     e.preventDefault();
+  //     performSearch();
+  //   }
+  // });
+  // searchBtn.addEventListener('click', () => {
+  //   performSearch();
+  // });
+
+  // function performSearch() {
+  //   const searchTerm = searchInput.value;
+
+  //   const getSneakers = (word, sneakers) => {
+  //     return sneakers.filter((s) => {
+  //       const regex = new RegExp(word, 'gi');
+  //       return s.title.match(regex);
+  //     });
+  //   };
+
+  //   const options = getSneakers(searchTerm, sneakers);
+  //   const html = options
+  //     .map((sneaker) => {
+  //       return `
+  //               <div class="sneakers__card">
+  //                   <button class="sneakers__favorit">
+  //                       <img src="./img/favorit.svg" alt="">
+  //                   </button>
+  //                   <div class="sneakers__img">
+  //                       <img src="${sneaker.imageUrl}" alt="sneakersIMG"/>
+  //                   </div>
+  //                   <div class="sneakers-title">${sneaker.title}</div>
+  //                   <div class="sneakers__lower">
+  //                       <div class="sneakers__price">
+  //                           <span class="sneakers-price-title">Цена:</span>
+  //                           <p class="sneakers-price">${sneaker.price} руб.</p>
+  //                       </div>
+  //                       <div class="sneacers__add">
+  //                           <button><img src="./img/addSneakers.svg" alt="">
+  //                           </button>
+  //                       </div>
+  //                   </div>
+  //               </div>
+  //           `;
+  //     })
+  //     .join('');
+  //   sneakersContent.innerHTML = html;
+  //   searchInput.value = ''; // Очистите поле ввода
+  // }
 
   const addFavoritBtn = document.querySelectorAll('.sneakers__favorit');
   const addSneakers = sneakersContent.querySelectorAll('.addSneakers');
@@ -148,9 +189,12 @@ const render = async () => {
         };
         // cartArr.push(newSneakers)
         renderCartSneakers(newSneakers);
+        console.log(newSneakers);
       } else {
         addBtn.innerHTML = `<img src="./img/addSneakers.svg" alt="">`;
         cartArr.pop(newSneakers);
+        console.log(newSneakers);
+
         console.log(cartArr);
         renderCartSneakers(cartArr);
       }
